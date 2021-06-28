e65f7e48ff1fda89b2e4405a68cc9bb6861af31b
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-06-27 21:43:07 -0300

    Exercício 4

---
+++ fundamentos/dia-1-javascript-primeiros-passos/ex04 .js
@@ -0,0 +1,14 @@
+/*Faça um programa que, dado um valor definido numa variável, retorne "positive" se esse valor for positivo, 
+"negative" se for negativo e "zero" caso contrário.*/
+
+let number = 4;
+
+if (number > 0) {
+  console.log("Positive");
+}
+else if (number < 0) {
+  console.log("Negative");
+}
+else {
+  console.log("ZERO");
+};
