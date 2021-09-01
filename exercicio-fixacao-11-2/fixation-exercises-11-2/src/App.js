152e2d56a4930a9294e940f0744c15687b91daba
Mariana Ferreira <marianadesouzaferreira01@gmail.com>
2021-08-31 21:50:18 -0300

    exercicio de fixaçao 1

---
+++ exercicio-fixacao-11-2/fixation-exercises-11-2/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Image from './Imagem';
+import catImg from './cat.webp'
+
+function App() {
+  return (
+    <div >
+      < Image source = "catImg" alternativeText = "Cute cat staring" />
+    </div>
+  );
+}
+
+export default App;
